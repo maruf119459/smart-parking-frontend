dec096cc666b08355a172172f68391005d94f8f0
maruf119459 <mdmohiuddinmaruf7@gmail.com>
2026-02-03 20:33:51 +0600

    Add PublicRoute.jsx

---
+++ src/route/PublicRoute.jsx
@@ -0,0 +1,8 @@
+// PublicRoute.jsx
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
+
+export default function PublicRoute({ children }) {
+  const { user } = useAuth();
+  return !user ? children : <Navigate to="/booking" />;
+}
